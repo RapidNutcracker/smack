e1737611fe4ddb011dc055e1dc79be1aff5db283
Patrick Curran <patrick.e.curran@gmail.com>
2018-11-15 16:33:55 -0500

    init commit

---
+++ Smack/Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Patrick Curran on 11/15/18.
+//  Copyright © 2018 Patrick Curran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
