6880590f8b8f50921df607ca6d42dc028d581801
Patrick Curran <patrick.e.curran@gmail.com>
2018-11-15 22:55:46 -0500

    implement login view UI

---
+++ Smack/Smack/Controller/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Smack
+//
+//  Created by Patrick Curran on 11/15/18.
+//  Copyright © 2018 Patrick Curran. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Patrick Curran on 11/15/18.
+//  Copyright © 2018 Patrick Curran. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
